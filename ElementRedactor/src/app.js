c6c95a4f006f1461c7e2289eeb63a92e06398ae0
DmitriyCody101 <108818620+DmitriyCody101@users.noreply.github.com>
2022-07-10 15:11:23 +0500

    i again remove my stupid in code

--- ElementRedactor/src/app.js
+++ ElementRedactor/src/app.js
@@ -4,9 +4,9 @@ const closeBtn = document.querySelector(".form button")
 const removeBtn = document.querySelector(".header img")
 const table = document.querySelector(".elements")
 
-createElement.addEventListener('click', form.style.display = 'block')
+createElement.addEventListener('click', () => form.style.display = 'block')
 
-closeBtn.addEventListener('click', form.style.display = 'none')
+closeBtn.addEventListener('click', () => form.style.display = 'none')
 
 document.querySelector("#submit").addEventListener('mouseup', function() {
     form.style.display = 'none'
